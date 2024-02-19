2d03128abef1c3503c03bfc683f0db8ae235d8ff
Enrique Segovia Vazquez <segoviavazquezenrique@gmail.com>
2024-02-19 14:43:33 +0000

    adding files for front

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 //import router from "./routes/tasks.routes.js";
 //import { port } from "./config.js";
 
 const port = 4000
 const app = express();
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
 
 // Middlewares
 app.use(cors());
