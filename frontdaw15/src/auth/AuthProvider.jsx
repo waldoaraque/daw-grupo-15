dc7faa3aac85f86ea74c419e209e115dda5307a9
Enrique Segovia Vazquez <segoviavazquezenrique@gmail.com>
2024-03-17 16:42:52 +0000

    Co-authored-by: Waldo Araque <waldoaraque@users.noreply.github.com>

--- frontdaw15/src/auth/AuthProvider.jsx
+++ frontdaw15/src/auth/AuthProvider.jsx
@@ -1,3 +1,5 @@
+/*
+
 import { useContext, createContext, useState, useEffect } from 'react'
 
 const AuthContext = createContext({
@@ -6,7 +8,7 @@ const AuthContext = createContext({
 
 export function AuthProvider({children}) {
 
-    const [isAuthenticated, setIsAuthenticated] = useState(true)
+    const [isAuthenticated, setIsAuthenticated] = useState(true  )
 
     return (
         <AuthContext.Provider value={{isAuthenticated}}>
@@ -14,6 +16,54 @@ export function AuthProvider({children}) {
         </AuthContext.Provider>
 
     )
+}
+
+export const useAuth = () => useContext(AuthContext);
+
+*/
+import React, { createContext, useContext, useState } from 'react';
+
+const AuthContext = createContext({
+  isAuthenticated: false,
+  handleAuth: () => Promise.resolve(),
+});
+
+export function AuthProvider({ children }) {
+  const API_HOST = 'http://localhost';
+  const API_PORT = 4000;
+
+  const handleAuth = async (email, contrasena) => {
+    try {
+      console.log('Usuario:', email, 'Contraseña:', contrasena);
+      const res = await fetch(`${API_HOST}:${API_PORT}/api/login`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, contrasena }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        const accessToken = data.access_token;
+        localStorage.setItem('token', accessToken);
+        setIsAuthenticated(true);
+      } else {
+        console.error('Inicio de sesión fallido');
+        setIsAuthenticated(false);
+      }
+    } catch (error) {
+      console.error('Error al realizar la solicitud:', error);
+      setIsAuthenticated(false);
+    }
+  };
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false); // Inicializar como falso
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, handleAuth }}>
+      {children}
+    </AuthContext.Provider>
+  );
 }
 
 export const useAuth = () => useContext(AuthContext);
---
+++ frontdaw15/src/config.js
@@ -0,0 +1,2 @@
+export const apiHost = process.env.API_HOST
+export const apiPort = process.env.API_PORT
